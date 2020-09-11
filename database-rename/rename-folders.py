5ceeaad8fbb2e3644af40217d80b2f3727cc19ca
NathanBitTheMoon <fuzzy.hazza.gld@protonmail.com>
2020-09-11 12:13:57 +1000

    I gotta keep pushing all of these changes to the server so just ignore these commits

--- database-rename/rename-folders.py
+++ database-rename/rename-folders.py
@@ -2,11 +2,11 @@ import sqlite3
 import shutil
 import os
 
-database = sqlite3.connect('metadata.db')
+database = sqlite3.connect('../../metadata.db')
 cursor = database.cursor()
 
-for i in os.listdir('./download/'):
-    folder = i.replace('./download/', '')
+for i in os.listdir('../../download/'):
+    folder = i.replace('../../download/', '')
     rows = cursor.execute(f"SELECT * FROM GAMES WHERE FULLID LIKE \"%{folder}\"")
     rows = rows.fetchall()
 
