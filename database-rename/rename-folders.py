4b29b1f942071ad9842566924ad09f11afe2c231
NathanBitTheMoon <fuzzy.hazza.gld@protonmail.com>
2020-09-11 11:58:06 +1000

    Folder rename script added

---
+++ database-rename/rename-folders.py
@@ -0,0 +1,14 @@
+import sqlite3
+import shutil
+import os
+
+database = sqlite3.connect('metadata.db')
+cursor = database.cursor()
+
+for i in os.listdir('./download/'):
+    folder = i.replace('./download/', '')
+    rows = cursor.execute(f"SELECT * FROM GAMES WHERE FULLID LIKE \"%{folder}\"")
+    rows = rows.fetchall()
+
+    for i in rows:
+        print(i)
