1d6f28e22732bdeb00fa7cecaf6a586d7c46fab4
NathanBitTheMoon <fuzzy.hazza.gld@protonmail.com>
2020-09-11 12:18:49 +1000

    Decided to take a different approach because SQLite3 is slow

--- database-rename/rename-folders.py
+++ database-rename/rename-folders.py
@@ -2,13 +2,13 @@ import sqlite3
 import shutil
 import os
 
-database = sqlite3.connect('../../metadata.db')
-cursor = database.cursor()
+file_rows = open('../../txt/games.txt').readlines()
 
 for i in os.listdir('../../download/'):
     folder = i.replace('../../download/', '')
-    rows = cursor.execute(f"SELECT * FROM GAMES WHERE FULLID LIKE \"%{folder}\"")
-    rows = rows.fetchall()
-
-    for i in rows:
-        print(i[1])
+    rename = ""
+    for i in file_rows:
+        if folder in i:
+            rename = i
+    
+    print(rename)
